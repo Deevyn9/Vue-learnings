6c36721a2f826d0bb3abfb04d1886ebfa8ca4d68
Divine Obeten <divineobeten9@gmail.com>
2024-03-08 21:08:02 +0100

    create navbar

---
+++ first-vue/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="navbar">
+        <div class="logo">
+            <h2>Vuebar</h2>
+        </div>
+
+        <ul>
+            <li v-for="link in Links" :key="link.id">
+                <a href={{link.link}}>{{ link.name }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+    setup() {
+        let Links = [
+            { name: "Home", link: "#" },
+            { name: "About", link: "#" },
+            { name: "Work", link: "#" },
+            { name: "Contact", link: "#" }
+        ]
+
+        return { Links }
+    }
+}
+
+
+</script>
