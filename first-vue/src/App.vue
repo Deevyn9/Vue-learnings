6646a532ae111403b02c8718ecd142a2edcdce0a
Divine Obeten <divineobeten9@gmail.com>
2024-03-08 21:06:00 +0100

    add navbar component

--- first-vue/src/App.vue
+++ first-vue/src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <div class="main-app">
+    <Navbar />
+  </div>
 </template>
+
+<script>
+import Navbar from "./components/NavBar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
