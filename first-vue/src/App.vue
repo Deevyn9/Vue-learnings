e00b79e150b7fcab2e3e3d02e3d5399e955353f9
Divine Obeten <divineobeten9@gmail.com>
2024-03-09 21:43:19 +0100

    add main page component to webpage

--- first-vue/src/App.vue
+++ first-vue/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="main-app">
     <Navbar />
+    <MainPage />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/NavBar.vue";
+import MainPage from "./components/MainPage.vue";
 
 export default {
   components: {
     Navbar,
+    MainPage
   },
 }
 </script>
