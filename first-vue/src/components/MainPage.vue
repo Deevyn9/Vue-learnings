3405f406ea38a660ea5dd0055a03ad04082858c4
Divine Obeten <divineobeten9@gmail.com>
2024-03-09 21:42:52 +0100

    add main page component

---
+++ first-vue/src/components/MainPage.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="main">
+        <h2>
+
+            <div>
+                <span>v</span>
+                <span>u</span>
+                <span>e</span>
+                <span>b</span>
+                <span>a</span>
+                <span>r</span>
+            </div>
+            <div>
+                <span>w</span>
+                <span class="i">i</span>
+                <span>t</span>
+                <span>h</span>
+            </div>
+            <div>
+                <span>l</span>
+                <span>a</span>
+                <span>v</span>
+                <span>e</span>
+                <span>n</span>
+                <span>d</span>
+                <span>e</span>
+                <span>r</span>
+            </div>
+            <div>
+                <span>l</span>
+                <span>o</span>
+                <span>o</span>
+                <span>k</span>
+                <span class="i">i</span>
+                <span>n</span>
+                <span>g</span>
+            </div>
+            <div>
+                <span>c</span>
+                <span>o</span>
+                <span>l</span>
+                <span>o</span>
+                <span>r</span>
+                <span>s</span>
+            </div>
+        </h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
